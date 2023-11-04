83dcdf5d9a647e7d7f8c67efc140479375da8900
ddmrc <63403841+ddmrc@users.noreply.github.com>
2023-11-04 13:15:16 +0100

    First Push
    Walls and GameMode for RandomSpawn

--- Source/Rhino_GJ2023/Rhino_GJ2023GameMode.cpp
+++ Source/Rhino_GJ2023/Rhino_GJ2023GameMode.cpp
@@ -4,6 +4,10 @@
 #include "Rhino_GJ2023PlayerController.h"
 #include "Rhino_GJ2023Character.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Kismet/Gameplaystatics.h"
+
+#include "Objects/Rhino_BreakableWall.h"
+#include "Objects/Rhino_SolidWall.h"
 
 ARhino_GJ2023GameMode::ARhino_GJ2023GameMode()
 {
@@ -22,5 +26,85 @@ ARhino_GJ2023GameMode::ARhino_GJ2023GameMode()
 	if(PlayerControllerBPClass.Class != NULL)
 	{
 		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
+
+void ARhino_GJ2023GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ScanAllObjects();
+	RandomSpawnObjects();
+}
+
+void ARhino_GJ2023GameMode::ScanAllObjects()
+{
+
+	InactiveSolidWalls.Empty();
+	InactiveBreakableWalls.Empty();
+
+	TArray<AActor*> Generic;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ARhino_SolidWall::StaticClass(), Generic);
+	for (int i = 0; i < Generic.Num(); i++)
+	{
+		if (Generic.IsValidIndex(i) && Generic[i] != nullptr)
+		{
+			if (ARhino_SolidWall* Wall = Cast<ARhino_SolidWall>(Generic[i]))
+			{
+				Wall->DeSpawnWall();
+				InactiveSolidWalls.Add(Wall);
+			}
+		}
+	}
+
+	Generic.Empty();
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ARhino_BreakableWall::StaticClass(), Generic);
+	for (int i = 0; i < Generic.Num(); i++)
+	{
+		if (Generic.IsValidIndex(i) && Generic[i] != nullptr)
+		{
+			if (ARhino_BreakableWall* Wall = Cast<ARhino_BreakableWall>(Generic[i]))
+			{
+				Wall->DeSpawnWall();
+				InactiveBreakableWalls.Add(Wall);
+			}
+		}
+	}
+
+
+}
+
+void ARhino_GJ2023GameMode::RandomSpawnObjects()
+{
+
+	ActiveSolidWalls.Empty();
+	ActiveBreakableWalls.Empty();
+
+	for (int i = 0; i < NumberSolidWalls; i++)
+	{
+		if (InactiveSolidWalls.IsEmpty())
+			break;
+
+		int32 Seed = FMath::RandRange(0, InactiveSolidWalls.Num() - 1);
+		if (InactiveSolidWalls.IsValidIndex(i) && InactiveSolidWalls[i] != nullptr)
+		{
+			InactiveSolidWalls[i]->SpawnWall();
+			ActiveSolidWalls.Add(InactiveSolidWalls[i]);
+			InactiveSolidWalls.RemoveAt(Seed);
+		}
+	}
+
+	for (int i = 0; i < NumberBreakableWalls; i++)
+	{
+		if (InactiveBreakableWalls.IsEmpty())
+			break;
+
+		int32 Seed = FMath::RandRange(0, InactiveBreakableWalls.Num() - 1);
+		if (InactiveBreakableWalls.IsValidIndex(i) && InactiveBreakableWalls[i] != nullptr)
+		{
+			InactiveBreakableWalls[i]->SpawnWall();
+			ActiveBreakableWalls.Add(InactiveBreakableWalls[i]);
+			InactiveBreakableWalls.RemoveAt(Seed);
+		}
 	}
 }
--- Source/Rhino_GJ2023/Rhino_GJ2023GameMode.h
+++ Source/Rhino_GJ2023/Rhino_GJ2023GameMode.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+
 #include "Rhino_GJ2023GameMode.generated.h"
+
+
+class ARhino_SolidWall;
+class ARhino_BreakableWall;
 
 UCLASS(minimalapi)
 class ARhino_GJ2023GameMode : public AGameModeBase
@@ -13,6 +18,33 @@ class ARhino_GJ2023GameMode : public AGameModeBase
 
 public:
 	ARhino_GJ2023GameMode();
+
+	UPROPERTY(EditAnywhere, Category = "MazeSpawn Options")
+		int32 NumberSolidWalls = 5;
+	UPROPERTY(EditAnywhere, Category = "MazeSpawn Options")
+		int32 NumberBreakableWalls = 5;
+	UPROPERTY(EditAnywhere, Category = "MazeSpawn Options")
+		int32 NumberFruits = 5;
+
+	UFUNCTION(BlueprintCallable)
+	void ScanAllObjects();
+	UFUNCTION(BlueprintCallable)
+	void RandomSpawnObjects();
+
+
+protected:
+
+	UPROPERTY()
+	TArray<ARhino_SolidWall*> ActiveSolidWalls;
+	UPROPERTY()
+	TArray<ARhino_BreakableWall*> ActiveBreakableWalls;
+	UPROPERTY()
+	TArray<ARhino_SolidWall*> InactiveSolidWalls;
+	UPROPERTY()
+	TArray<ARhino_BreakableWall*> InactiveBreakableWalls;
+	//TArray<ARhino_SolidWall*> Fruits;
+	virtual void BeginPlay() override;
+
 };
 
 
