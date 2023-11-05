ea34bdcffd7ebfb2d5f04c0f18afd0ab5ba68d62
Shenkrad <58443488+Shenkrad@users.noreply.github.com>
2023-11-05 22:19:01 +0100

    Disabled point and click movement
    Quoted functions that allows player to move with point and click action to test movement.

--- Source/Rhino_GJ2023/Rhino_GJ2023PlayerController.cpp
+++ Source/Rhino_GJ2023/Rhino_GJ2023PlayerController.cpp
@@ -15,10 +15,10 @@
 
 ARhino_GJ2023PlayerController::ARhino_GJ2023PlayerController()
 {
-	bShowMouseCursor = true;
-	DefaultMouseCursor = EMouseCursor::Default;
-	CachedDestination = FVector::ZeroVector;
-	FollowTime = 0.f;
+	// bShowMouseCursor = true;
+	// DefaultMouseCursor = EMouseCursor::Default;
+	// CachedDestination = FVector::ZeroVector;
+	// FollowTime = 0.f;
 }
 
 void ARhino_GJ2023PlayerController::BeginPlay()
@@ -44,10 +44,10 @@ void ARhino_GJ2023PlayerController::SetupInputComponent()
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
 	{
 		// Setup mouse input events
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &ARhino_GJ2023PlayerController::OnInputStarted);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &ARhino_GJ2023PlayerController::OnSetDestinationTriggered);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &ARhino_GJ2023PlayerController::OnSetDestinationReleased);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &ARhino_GJ2023PlayerController::OnSetDestinationReleased);
+		// EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &ARhino_GJ2023PlayerController::OnInputStarted);
+		// EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &ARhino_GJ2023PlayerController::OnSetDestinationTriggered);
+		// EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &ARhino_GJ2023PlayerController::OnSetDestinationReleased);
+		// EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &ARhino_GJ2023PlayerController::OnSetDestinationReleased);
 
 		// Setup dash event
 		EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Started, this, &ARhino_GJ2023PlayerController::OnDashTriggered);
@@ -57,49 +57,49 @@ void ARhino_GJ2023PlayerController::SetupInputComponent()
 	}
 }
 
-void ARhino_GJ2023PlayerController::OnInputStarted()
-{
-	StopMovement();
-}
+// void ARhino_GJ2023PlayerController::OnInputStarted()
+// {
+// 	StopMovement();
+// }
 
 // Triggered every frame when the input is held down
-void ARhino_GJ2023PlayerController::OnSetDestinationTriggered()
-{
-	// We flag that the input is being pressed
-	FollowTime += GetWorld()->GetDeltaSeconds();
+// void ARhino_GJ2023PlayerController::OnSetDestinationTriggered()
+// {
+// 	// We flag that the input is being pressed
+// 	FollowTime += GetWorld()->GetDeltaSeconds();
 	
-	// We look for the location in the world where the player has pressed the input
-	FHitResult Hit;
-	bool bHitSuccessful = false;
-	bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
+// 	// We look for the location in the world where the player has pressed the input
+// 	FHitResult Hit;
+// 	bool bHitSuccessful = false;
+// 	bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
 
-	// If we hit a surface, cache the location
-	if (bHitSuccessful)
-	{
-		CachedDestination = Hit.Location;
-	}
+// 	// If we hit a surface, cache the location
+// 	if (bHitSuccessful)
+// 	{
+// 		CachedDestination = Hit.Location;
+// 	}
 	
-	// Move towards mouse pointer or touch
-	APawn* ControlledPawn = GetPawn();
-	if (ControlledPawn != nullptr)
-	{
-		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
-		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
-	}
-}
+// 	// Move towards mouse pointer or touch
+// 	APawn* ControlledPawn = GetPawn();
+// 	if (ControlledPawn != nullptr)
+// 	{
+// 		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
+// 		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+// 	}
+// }
 
-void ARhino_GJ2023PlayerController::OnSetDestinationReleased()
-{
-	// If it was a short press
-	if (FollowTime <= ShortPressThreshold)
-	{
-		// We move there and spawn some particles
-		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
-		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
-	}
+// void ARhino_GJ2023PlayerController::OnSetDestinationReleased()
+// {
+// 	// If it was a short press
+// 	if (FollowTime <= ShortPressThreshold)
+// 	{
+// 		// We move there and spawn some particles
+// 		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
+// 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
+// 	}
 
-	FollowTime = 0.f;
-}
+// 	FollowTime = 0.f;
+// }
 
 void ARhino_GJ2023PlayerController::OnDashTriggered()
 {
--- Source/Rhino_GJ2023/Rhino_GJ2023PlayerController.h
+++ Source/Rhino_GJ2023/Rhino_GJ2023PlayerController.h
@@ -32,9 +32,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
 	
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
-	class UInputAction* SetDestinationClickAction;
+	// /** Jump Input Action */
+	// UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	// class UInputAction* SetDestinationClickAction;
 
 	/* Dash Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
@@ -51,7 +51,7 @@ public:
 
 protected:
 	/** True if the controlled character should navigate to the mouse cursor. */
-	uint32 bMoveToMouseCursor : 1;
+	// uint32 bMoveToMouseCursor : 1;
 
 	virtual void SetupInputComponent() override;
 	
@@ -66,11 +66,11 @@ protected:
 	void CharacterMovement(const FInputActionValue &Value);
 
 private:
-	FVector CachedDestination;
+	// FVector CachedDestination;
 	
 	FTimerHandle DashTimerHandle;
 
-	float FollowTime; // For how long it has been pressed
+	// float FollowTime; // For how long it has been pressed
 
 	UPROPERTY()
 	ARhino_GJ2023Character* ControlledCharacter;
